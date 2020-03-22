196a59a62437b24e9ebb451465c3e0292b54233c
Julian <jdmesalosada@gmail.com>
2020-03-22 15:37:26 +0100

    first commit

---
+++ src/main/java/config/AppConfig.java
@@ -0,0 +1,11 @@
+package main.java.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+
+
+}
