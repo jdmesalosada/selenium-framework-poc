cae785faad73e2b337076285ab2c4b81bcbe3b59
Julian <jdmesalosada@gmail.com>
2020-04-08 09:24:32 +0200

    added listener

--- src/test/java/com/mesaj/app/util/reporting/JListener.java
+++ src/test/java/com/mesaj/app/util/reporting/JListener.java
@@ -1,10 +1,10 @@
 package com.mesaj.app.util.reporting;
 
 
-import io.cucumber.plugin.EventListener;
+import io.cucumber.plugin.ConcurrentEventListener;
 import io.cucumber.plugin.event.*;
 
-public class JListener implements EventListener {
+public class JListener implements ConcurrentEventListener {
 
     public void onTestCaseStarted(TestCaseStarted event) {
         TestCase testCase = event.getTestCase();
@@ -22,9 +22,26 @@ public class JListener implements EventListener {
         }
     }
 
+    private void onTestStepFinished(final TestStepFinished event) {
+
+        TestStep tonces = event.getTestStep();
+
+        System.out.println("Finished " + tonces.getCodeLocation());
+        Result result = event.getResult();
+
+
+        if (result.getStatus() == Status.FAILED) {
+            final Throwable error = result.getError();
+            error.printStackTrace();
+        }
+    }
+
+
+
     @Override
     public void setEventPublisher(EventPublisher eventPublisher) {
         eventPublisher.registerHandlerFor(TestCaseStarted.class, this::onTestCaseStarted);
         eventPublisher.registerHandlerFor(TestCaseFinished.class, this::onTestCaseFinished);
+        eventPublisher.registerHandlerFor(TestStepFinished.class, this::onTestStepFinished);
     }
 }
