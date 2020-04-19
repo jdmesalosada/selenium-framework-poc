8aa2f5605f1bfebc93458e53992c9cdf3318c105
Julian <jdmesalosada@gmail.com>
2020-04-19 11:24:45 +0200

    added remote webdriver

--- src/test/java/CucumberTest.java
+++ src/test/java/CucumberTest.java
@@ -15,8 +15,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",
         plugin = {"de.monochromata.cucumber.report.PrettyReports:target/cucumber",
-                "pretty", "json:target/cucumber-reports/Cucumber.json",
-                "com.mesaj.app.util.reporting.JReporter",
+                "pretty", "json:target/cucumber-reports/Cucumber.json"
+                //"com.mesaj.app.util.reporting.JReporter",
                 //"junit:target/cucumber-reports/Cucumber.xml",
                 //"html:target/cucumber-reports"
         }
